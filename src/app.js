fb60ca27cf1ca27181c6b1efca9cf506699dc062
arpan7sarkar <arpansakar@gmail.com>
2025-08-16 02:38:01 +0530

    learning about the http methods GET,PUT,POST,PATCH,DELETE

--- src/app.js
+++ src/app.js
@@ -5,20 +5,36 @@ const app=express();
 //     res.send("Hello from server");//server is responsing
 // })
 
+//in the industry we dont use app.use cause in this format get/post/put/delete all works same so we use app.get
+app.get("/hello",(req,res)=>{
+    res.send("Hello from a get rquest");
+})
+app.post("/hello",(req,res)=>{
+    res.send("Hello from a post rquest");
+})
+app.patch("/hello",(req,res)=>{
+    res.send("Hello from a patch rquest");
+})
+app.put("/hello",(req,res)=>{
+    res.send("Hello from a put rquest");
+})
+app.delete("/hello",(req,res)=>{
+    res.send("Hello from a delete rquest");
+})
+
+
 app.use("/test/2",(req,res)=>{
-    res.send("Hello from test2");//server is responsing
+    res.send("Hello from test2");
 })
 app.use("/test",(req,res)=>{
-    res.send("Hello from test");//server is responsing
+    res.send("Hello from test");
 })
-app.use("/hello",(req,res)=>{
-    res.send("Hello from World");//server is responsing
-})
+
 app.use("/routeName",(req,res)=>{
-    res.send("Hello from server");//server is responsing
+    res.send("Hello from server");
 })
 app.use("/",(req,res)=>{
-    res.send("Hello from Homepage");//server is responsing
+    res.send("Hello from Homepage");
 })
 app.listen(7777,()=>{
     console.log("server has been succesfullly listening at http://localhost:7777/");
