915f3cf0559929e5aeef53803b70dfc41b42512d
arpan7sarkar <arpansakar@gmail.com>
2025-08-16 01:23:21 +0530

    Create a server using Express now lets learn another things

---
+++ src/app.js
@@ -0,0 +1,22 @@
+console.log("Starting a new project");
+const express=require('express');
+const app=express();
+// app.use("/routeName",(req,res)=>{
+//     res.send("Hello from server");//server is responsing
+// })
+app.use("/",(req,res)=>{
+    res.send("Hello from Homepage");//server is responsing
+})
+app.use("/test",(req,res)=>{
+    res.send("Hello from test");//server is responsing
+})
+app.use("/hello",(req,res)=>{
+    res.send("Hello from World");//server is responsing
+})
+app.use("/routeName",(req,res)=>{
+    res.send("Hello from server");//server is responsing
+})
+app.listen(7777,()=>{
+    console.log("server has been succesfullly listening at http://localhost:7777/");
+    
+});//give port here
