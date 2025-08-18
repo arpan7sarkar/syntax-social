83be29fad8165f9b411e65c7e2587e739f77e263
arpan7sarkar <arpansakar@gmail.com>
2025-08-18 20:45:32 +0530

    playing with the route handlers

--- src/app.js
+++ src/app.js
@@ -1,12 +1,18 @@
 console.log("Starting a new project");
 const express=require('express');
 const app=express();
-//you can use multiple route handelrs at a time
+//you can use multiple route handelrs at a time and here res.send works like return function (mostly but not purely true) next still works after using res.send (there are bugs nothing is sure here)
 app.use("/hello",(req,res,next)=>{
     console.log("Hello from a post rquest");
     next();
 },(req,res,next)=>{
+    next();
+    console.log("Hello 2 bro");
+    next();
+},(req,res,next)=>{
     res.send("Response is ending here");
+},(req,res,next)=>{
+    console.log("final console.log");
 })
 
 app.listen(7777,()=>{
