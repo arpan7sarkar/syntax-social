34746feff4bf6c85d9e0a1a2823ca267914d7f31
arpan7sarkar <arpansakar@gmail.com>
2025-08-21 01:36:59 +0530

    formating the files

--- src/app.js
+++ src/app.js
@@ -1,15 +1,14 @@
 console.log("Starting a new project");
 require("dotenv").config();
 const express=require('express');
-const {connectDB}=require("../config/database.js");
+const {connectDB}=require("./config/database.js");
 
 const app=express();
-const {userVerification,adminVerification}=require("../utils/index.js");
+const {userVerification,adminVerification}=require("./utils/index.js");
 connectDB().then(()=>{
     console.log("MongoDb setup done");
     app.listen(7777,()=>{
     console.log("server has been succesfullly listening at http://localhost:7777/");
-    
 });
 }).catch((err)=>{
     console.log(err)
