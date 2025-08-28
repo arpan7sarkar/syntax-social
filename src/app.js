195d8a59070985f6db9b5f1a6fb217b3b5fd7d7e
arpan7sarkar <arpansakar@gmail.com>
2025-08-29 00:09:33 +0530

    Refactor user model schema to include validation and default values

--- src/app.js
+++ src/app.js
@@ -1,78 +1,82 @@
 console.log("Starting a new project");
 require("dotenv").config();
-const express=require('express');
-const {connectDB}=require("./config/database.js");
-const {userModel}=require("./model/user.js");
-const app=express();
-const {userVerification,adminVerification}=require("./utils/index.js");
+const express = require("express");
+const { connectDB } = require("./config/database.js");
+const { userModel } = require("./model/user.js");
+const app = express();
+const { userVerification, adminVerification } = require("./utils/index.js");
 app.use(express.json());
-app.post("/signup",async (req,res)=>{
-    // const user =new userModel({
-    //     fName:"Shakal",
-    //     lName:"Khiladi",
-    //     emailId:"Shaku@gmaill.com",
-    //     password:"theOnePiece",
-    //     age:106
-    // })
-    const user=new userModel(req.body);
-    
-    try {
-        await user.save();
-        res.send("New user had been created")
-    } catch (error) {
-        res.status(400).send("The user is not saved" + error);
-    }
-})
-//getting a particuler user using their email address
-app.get("/user",async (req,res)=>{
-    const email=req.body.emailId;
-    const user=await userModel.find({emailId:email})
-    res.send(user);
-})
-app.delete("/user",async (req,res)=>{
-    const id=req.body._id;
-    try {
-        const user=await userModel.findByIdAndDelete(id)
-        res.send("user is succesfully deleted");
-    } catch (error) {
-        console.log(error);
-    }
-})
+app.post("/signup", async (req, res) => {
+  // const user =new userModel({
+  //     fName:"Shakal",
+  //     lName:"Khiladi",
+  //     emailId:"Shaku@gmaill.com",
+  //     password:"theOnePiece",
+  //     age:106
+  // })
+  const user = new userModel(req.body);
 
-app.patch("/updateById",async (req,res)=>{
-    const id=req.body._id;
-    try {
-        await userModel.findByIdAndUpdate(id,req.body);
-        res.send("Id has succesfully updated");
-    } catch (error) {
-        res.status(400).send("The is is not found ");
-    }
-})
-app.get("/userById",async (req,res)=>{
-    const id=req.body._id;
-    const user=await userModel.findById(id);
-    try {
-        res.send(user);
-    } catch (error) {
-        res.status(400).send("The is is not found ");
-    }
-})
-//ALl the users from the db 
-app.get("/feed", async (req,res)=>{
-    const users=await userModel.find({});//this will showcase all the users/documents that are avilable 
-    if(!users){
-        res.status(400).send("THere are no user ")
-    }else{
-        console.log("User found succesfully");
-        res.send(users);
-    }
-})
-connectDB().then(()=>{
-    console.log("MongoDb setup done");
-    app.listen(7777,()=>{
-    console.log("server has been succesfullly listening at http://localhost:7777/");
+  try {
+    await user.save();
+    res.send("New user had been created");
+  } catch (error) {
+    res.status(400).send("The user is not saved" + error);
+  }
 });
-}).catch((err)=>{
-    console.log(err)
-})
+//getting a particuler user using their email address
+app.get("/user", async (req, res) => {
+  const email = req.body.emailId;
+  const user = await userModel.find({ emailId: email });
+  res.send(user);
+});
+app.delete("/user", async (req, res) => {
+  const id = req.body._id;
+  try {
+    const user = await userModel.findByIdAndDelete(id);
+    res.send("user is succesfully deleted");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.patch("/updateById", async (req, res) => {
+  const id = req.body._id;
+  try {
+    await userModel.findByIdAndUpdate(id, req.body,runValidator="true");//without runValidator the validation schemas will not gonna work here
+    res.send("Id has succesfully updated");
+  } catch (error) {
+    res.status(400).send("Update failed" + error.message);
+  }
+});
+app.get("/userById", async (req, res) => {
+  const id = req.body._id;
+  const user = await userModel.findById(id);
+  try {
+    res.send(user);
+  } catch (error) {
+    res.status(400).send("The is is not found ");
+  }
+});
+//ALl the users from the db
+app.get("/feed", async (req, res) => {
+  const users = await userModel.find({}); //this will showcase all the users/documents that are avilable
+  if (!users) {
+    res.status(400).send("THere are no user ");
+  } else {
+    console.log("User found succesfully");
+    res.send(users);
+  }
+});
+connectDB()
+  .then(() => {
+    console.log("MongoDb setup done");
+    app.listen(7777, () => {
+      console.log(
+        "server has been succesfullly listening at http://localhost:7777/"
+      );
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 //give port here
--- src/model/user.js
+++ src/model/user.js
@@ -1,13 +1,26 @@
-const mongoose=require('mongoose');
-const userSchema= new mongoose.Schema({
-    fName:{type:String},
-    lName:{
-        type:String
-    },
-    emailId: {type:String},
-    password:{type:String},
-    age:{type:Number},
-    gender:{type:String}
-})
-const userModel=mongoose.model("sser",userSchema);
-module.exports={userModel}
+const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema({
+  fName: {
+    type: String,
+    minlength: 3,
+    required: true,
+  },
+  lName: {
+    type: String,
+  },
+  emailId: { type: String, required: true , unique:true,lowercase:true,trim:true,},
+  password: { type: String, required: true },
+  age: { type: Number },
+  gender: { type: String ,validate(value){
+    if(value.toLowerCase()!=("male"||"female"||"other")){
+        throw new Error("NOt a valide gender chose widely ");
+    }
+  }},
+  photoUrl:{type:String},
+  about:{type:String,default:"Hey there I am using Syntax social"},
+  skills:{type:[String]}
+},{
+    timestamps:true//this will add the timestamp (created at and updated at)when the user is signed up 
+});
+const userModel = mongoose.model("sser", userSchema);
+module.exports = { userModel };
