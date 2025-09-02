419772aa0410447c237736b3095c1b2122e62a3b
arpan7sarkar <arpansakar@gmail.com>
2025-09-02 13:05:46 +0530

    removed dummy practise api's

--- src/app.js
+++ src/app.js
@@ -11,10 +11,9 @@ const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
 const { userAuth } = require("./utils/middlewares/auth.js");
 // Always remeber to use‼️await‼️‼️ leura bara
-
-
 app.use(express.json());
 app.use(cookieParser());
+
 app.post("/signup", async (req, res) => {
   const { fName, lName, emailId, password, age, skills, gender, about } =
     req.body;
@@ -64,11 +63,6 @@ app.post("/login", async (req, res) => {
 });
 app.get("/profile",userAuth, async (req, res) => {
   try {
-    // const cookie = req.cookies;
-    // const { token } = cookie;
-    // const decoded = await jwt.verify(token, "MyJwtSecret@arpan");
-    // const { _id } = decoded;
-    // const user = await userModel.findById(_id);
    const user=req.user;
     res.send(user);
   } catch (error) {
@@ -76,71 +70,6 @@ app.get("/profile",userAuth, async (req, res) => {
   }
 });
 
-//getting a particuler user using their email address
-app.get("/user", async (req, res) => {
-  const email = req.body.emailId;
-  const user = await userModel.find({ emailId: email });
-  res.send(user);
-});
-app.delete("/user", async (req, res) => {
-  const id = req.body._id;
-  try {
-    const user = await userModel.findByIdAndDelete(id);
-    res.send("user is succesfully deleted");
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-app.patch("/user/:userId", async (req, res) => {
-  const id = req.params.userId;
-  try {
-    const allowedUpdates = [
-      "_id",
-      "fName",
-      "lName",
-      "password",
-      "age",
-      "skills",
-    ];
-    const isUpdateAllowed = Object.keys(req.body).every((key) =>
-      allowedUpdates.includes(key)
-    );
-    if (!isUpdateAllowed) {
-      throw new Error("Update is not allowed for these vlaues");
-    }
-    if (req.body?.skills?.length > 5) {
-      throw new Error("You can't add more than 5 skills");
-    }
-    await userModel.findByIdAndUpdate(
-      id,
-      req.body,
-      (runValidator = "true" && !id)
-    ); //without runValidator the validation schemas will not gonna work here
-    res.send("Id has succesfully updated");
-  } catch (error) {
-    res.status(400).send("Update failed " + error.message);
-  }
-});
-app.get("/userById", async (req, res) => {
-  const id = req.body._id;
-  const user = await userModel.findById(id);
-  try {
-    res.send(user);
-  } catch (error) {
-    res.status(400).send("The is is not found ");
-  }
-});
-//ALl the users from the db
-app.get("/feed", async (req, res) => {
-  const users = await userModel.find({}); //this will showcase all the users/documents that are avilable
-  if (!users) {
-    res.status(400).send("THere are no user ");
-  } else {
-    console.log("User found succesfully");
-    res.send(users);
-  }
-});
 connectDB()
   .then(() => {
     console.log("MongoDb setup done");
