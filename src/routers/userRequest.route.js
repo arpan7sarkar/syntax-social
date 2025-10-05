0a69482e71cdb10fd89ccd7f29b26cf8024d836a
arpan7sarkar <arpansakar@gmail.com>
2025-10-05 14:16:30 +0530

    added feed api

--- src/routers/userRequest.route.js
+++ src/routers/userRequest.route.js
@@ -2,7 +2,8 @@ const express = require("express");
 const userRouter = express.Router();
 const { userAuth } = require("../utils/middlewares/auth");
 const { connectionModel } = require("../model/connectionRequest");
-const USER_PUBLIC_DATA= "fName lName photoUrl about age";
+const USER_PUBLIC_DATA = "fName lName photoUrl about age";
+const {userModel}=require("../model/user.js")
 userRouter.get("/user/request/recived", userAuth, async (req, res) => {
   try {
     const loggedinUserId = req.user._id;
@@ -11,15 +12,15 @@ userRouter.get("/user/request/recived", userAuth, async (req, res) => {
         toUserId: loggedinUserId,
         status: "interested",
       })
-      .populate("fromUserId",USER_PUBLIC_DATA); //after adding ref in a model, we can use populate here that means we can use populate to get their data
+      .populate("fromUserId", USER_PUBLIC_DATA); //after adding ref in a model, we can use populate here that means we can use populate to get their data
     //always remember to restrict populate else your important data will be exposed
 
     // .populate("fromUserId",["fName","lName"]); we can also write populate in this way
 
-    const data=request.map((item)=>item.fromUserId);
+    const data = request.map((item) => item.fromUserId);
     res.json({
       message: "Data feched succesfully\n",
-      data
+      data,
     });
   } catch (error) {
     console.log(error);
@@ -31,27 +32,58 @@ userRouter.get("/user/request/recived", userAuth, async (req, res) => {
 userRouter.get("/user/connections", userAuth, async (req, res) => {
   try {
     const loggedinUser = req.user;
-    const myConnection =await connectionModel.find({
-      $or: [
-        { toUserId: loggedinUser._id, status: "accepted" },
-        { fromUserId: loggedinUser._id, status: "accepted" },
-      ],
-    }).populate("fromUserId", USER_PUBLIC_DATA).populate("toUserId", USER_PUBLIC_DATA);
-    const data=myConnection.map((item)=>{
-      if(item.fromUserId.toString === loggedinUser._id.toString()){
-        return item.toUserId
+    const myConnection = await connectionModel
+      .find({
+        $or: [
+          { toUserId: loggedinUser._id, status: "accepted" },
+          { fromUserId: loggedinUser._id, status: "accepted" },
+        ],
+      })
+      .populate("fromUserId", USER_PUBLIC_DATA)
+      .populate("toUserId", USER_PUBLIC_DATA);
+    const data = myConnection.map((item) => {
+      if (item.fromUserId.toString === loggedinUser._id.toString()) {
+        return item.toUserId;
       }
-      return item.fromUserId
+      return item.fromUserId;
     });
     res.json({
-      message:"Your connnection requests are",data
-      
-    })
+      message: "Your connnection requests are",
+      data,
+    });
   } catch (error) {
     console.log(error.message);
     res.status(400).json({
       message: "facing some issues",
     });
+  }
+});
+userRouter.get("/feed", userAuth, async (req, res) => {
+  try {
+    const loggedinUser = req.user;
+    const connections = await connectionModel
+      .find({
+        $or: [{ toUserId: loggedinUser._id }, { fromUserId: loggedinUser._id }],
+      })
+      .select("fromUserId toUserId");
+  
+    const hideUserFromFeed = new set();
+    connections.forEach((req) => {
+      hideUserFromFeed.add(req.fromUserId.toString());
+      hideUserFromFeed.add(req.toUserId.toString());
+    });
+  
+    const users = await userModel.find({
+      $and: [
+        { _id: { $nin: Array.from(hideUserFromFeed) } },//nin menas not in this 
+        { _id: { $ne: loggedinUser._id } },//ne means not be equal to
+      ],
+    }).select(USER_PUBLIC_DATA);
+  } catch (error) {
+    res.status(400).json({
+      message:"Facing some errors here",
+      error:error.message
+    })
   }
 });
 module.exports = { userRouter };
