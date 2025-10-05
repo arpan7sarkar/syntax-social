ec28fbb91ef342666b076c0437fe4082e71bcf1e
arpan7sarkar <arpansakar@gmail.com>
2025-10-05 14:35:54 +0530

    sanitized the limit so it can't get break by big values from the user side

--- src/routers/userRequest.route.js
+++ src/routers/userRequest.route.js
@@ -63,7 +63,8 @@ userRouter.get("/feed", userAuth, async (req, res) => {
   try {
     const loggedinUser = req.user;
     const page = parseInt(req.query.page) || 1;
-    const limit = parseInt(req.query.limit) || 10;
+    let limit = parseInt(req.query.limit) || 10;
+    limit= limit>50?50:limit;
     const skip = (page - 1) * limit;
 
     const connections = await connectionModel
